f1e16cbcc970c056ae4cdddd090e26c43b9c0416
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2023-04-03 02:47:06 +0530

    Added Memory Game to CP

---
+++ database/migrations/2023_04_03_129313_h5p_add_memory_game_to_cp_semantics.php
@@ -0,0 +1,30 @@
+<?php
+
+use Database\Seeders\H5PAddMemoryGameToCpSemanticsSeeder;
+use Illuminate\Database\Migrations\Migration;
+
+class H5PAddMemoryGameToCpSemantics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PAddMemoryGameToCpSemanticsSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
